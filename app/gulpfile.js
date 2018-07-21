26a65180527b2f1f691e3dff01c502b611edb319
ryoju-ohata <ryoju@example.com>
2018-07-21 15:30:54 +0900

    Fix gulp

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,40 +1,40 @@
 var browserify = require('browserify'),
-    gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    browserSync = require('browser-sync'),
-    combineMq = require('gulp-combine-mq'),
-    notify = require("gulp-notify"),
-    plumber = require('gulp-plumber');
+  gulp = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  browserSync = require('browser-sync'),
+  combineMq = require('gulp-combine-mq'),
+  notify = require("gulp-notify"),
+  plumber = require('gulp-plumber');
 
 var entryPoint = './src/js/script.js',
-    browserDir = './dist',
-    sassWatchPath = './src/scss/**/*.scss',
-    jsWatchPath = './src/**/*.js',
-    htmlWatchPath = './**/*.html';
+  browserDir = './dist',
+  sassWatchPath = './src/scss/**/*.scss',
+  jsWatchPath = './src/**/*.js',
+  htmlWatchPath = './**/*.html';
 
 gulp.task('js', function () {
-    return browserify(entryPoint, {debug: true, extensions: ['es6']})
-        .transform("babelify", {presets: ["es2015"]})
-        .bundle()
-        .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
-        .pipe(source('js/script.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/'))
-        .pipe(browserSync.reload({stream: true}));
+  return browserify(entryPoint, {debug: true, extensions: ['es6']})
+    .transform("babelify", {presets: ["es2015"]})
+    .bundle()
+    .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+    .pipe(source('js/script.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function () {
-    const config = {
-        server: {baseDir: browserDir}
-    };
+  const config = {
+    server: {baseDir: browserDir}
+  };
 
-    return browserSync(config);
+  return browserSync(config);
 });
 
 gulp.task('sass', function () {
@@ -43,10 +43,11 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-        browsers: ['last 2 versions']
+      browsers: ['last 1 version', 'iOS >= 8.1', 'Android >= 4.4'],
+      cascade: false
     }))
     .pipe(combineMq({
-        beautify: false
+      beautify: false
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/css/'))
@@ -54,12 +55,12 @@ gulp.task('sass', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(jsWatchPath, ['js']);
-    gulp.watch(sassWatchPath, ['sass']);
-    gulp.watch(htmlWatchPath, function () {
-        return gulp.src('')
-            .pipe(browserSync.reload({stream: true}))
-    });
+  gulp.watch(jsWatchPath, ['js']);
+  gulp.watch(sassWatchPath, ['sass']);
+  gulp.watch(htmlWatchPath, function () {
+    return gulp.src('')
+      .pipe(browserSync.reload({stream: true}))
+  });
 });
 
 gulp.task('default', ['js', 'sass', 'watch', 'browser-sync']);
